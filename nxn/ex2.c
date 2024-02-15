4baec4f73346a824a5baf63eb8eedb92baa14841
Samvel Petrosyan <loidnoir@gmail.com>
2024-02-15 10:01:47 +0400

    Update ex2.c

--- nxn/ex2.c
+++ nxn/ex2.c
@@ -1,47 +1,32 @@
-#include <stdio.h>
-int main() {
-    int n, m, i, j;
-    printf("Input n&m=");
-    scanf("%d%d",&n,&m);
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 
-    int b[n][m];
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < m; j++) {
-            scanf("%d", &b[i][j]);
-        }
+int main()
+{
+    int n, i, j, a[30][30], max;
+    
+    srand(time(0));
+    
+    printf("Enter natural number for n( n < 30 ) ");
+    scanf("%d", &n);
+    
+    for ( i = 0; i < n; i ++ )
+    {
+     for ( j = 0; j < n; j ++ )
+     {
+         a[i][j] = 50 - rand()%101;
+         printf("%d ", a[i][j]);
+     }
+     printf("\n");
     }
-
-    int k = 0;
-    int p = 0;
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < m; j++) {
-            if(b[i][j]>b[k][p]){
-            b[i][j]=b[k][p];
-            k=i;
-            p=j;
-        }
-    }
-}
-
-for (i = 0; i < n; i++) {
-        for (j = 0; j < m; j++) {
-        if(b[i][j]==b[k][p]){
-         printf("[%d%d]",i,j);
-        }
-}printf("\n");
-}
-
-            
-int S = 0;
-for (j = 0; j < m; j++){
-S+=b[i][j];
-}
-
- int F=0;
- for (i = 0; i < n; i++) {
- F+=b[i][j];
- }
- 
- printf("result=%d",S+F);
-    return 0;
+    
+    max = a[1][0];
+    for ( i = 2; i < n; i ++ )
+     for ( j = 0; j < i; j ++ )
+     if ( a[i][j] > max ) max = a[i][j];
+     
+     printf("Sum of the maximum is %d", max);
+     
+     return 0;
 }
