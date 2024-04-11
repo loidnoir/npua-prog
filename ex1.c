f74dd53e50ed97db73ac54cd3a2ef6e22437c9ab
Samvel Petrosyan <loidnoir@gmail.com>
2024-04-11 09:56:25 +0400

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -27,26 +27,7 @@ int main() {
     scanf("%d", &n);
 
     struct Person people[n];
-
-    // strcpy(people[0].AAH, "Armen");
-    // people[0].address.apartment = 12;
-    // people[0].address.building = 12;
-    // strcpy(people[0].address.city, "Vanadzor");
-    // strcpy(people[0].address.street, "Qochar");
-    // people[0].birthYear = 2003;
-    // strcpy(people[0].phone.cell, "095121212");
-    // strcpy(people[0].phone.home, "095121212");
-
-    // strcpy(people[1].AAH, "Alex");
-    // people[1].address.apartment = 12;
-    // people[1].address.building = 12;
-    // strcpy(people[1].address.city, "Yerevan");
-    // strcpy(people[1].address.street, "Qochar");
-    // people[1].birthYear = 2003;
-    // strcpy(people[1].phone.cell, "095121212");
-    // strcpy(people[1].phone.home, "095121212");
-
-
+    
     for (int i = 0; i < n; i++) {
         printf("Input the %d'th person's AAH: ", i+1);
         char[30] AAH;
